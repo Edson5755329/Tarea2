c3960cd93aba09d4233ea85748c9c06180c09a45
edson eduardo <edson5755329@gmail.com>
2022-06-04 23:59:07 -0500

    promedio

--- promedio.js
+++ promedio.js
@@ -1,13 +1,15 @@
-function calcularMediaAritmetica(lista) {
-    // let sumaLista = 0;
-    // for (let i = 0; i < lista.length; i++) {
-    //     sumaLista = sumaLista + lista[i];
-    // }
-    const sumaLista = lista.reduce(
-        function(valorAcumulado = 0,nuevoElemento){
-            return valorAcumulado+nuevoElemento;
-        }
-    );
-    const promedioLista = sumaLista / lista.length;
-    return promedioLista;
+function promedio(a,b,c){
+    return (a*b*c);
+ }
+function calcularPromedio() {
+    const vr1 = document.getElementById("t1"); 
+    const valor = vr1.value;
+    const vr2 = document.getElementById("t2"); 
+    const valor2 = vr2.value;
+    const vr3 = document.getElementById("t3"); 
+    const valor3 = vr3.value;
+    const promedio = promedio(parseFloat(valor),parseFloat(valor2),parseFloat(valor3));
+    const resultado = document.getElementById("Promedio");
+    resultado.innerText = `El promedio es : ${promedio}`;
 }
+
